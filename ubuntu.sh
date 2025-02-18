58755cec21bac36bb7d1793b90c5794f748dbf5d
Danilo Ancilotto <daniloancilotto@gmail.com>
2025-02-18 17:45:23 -0300

    Updated ventoy app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "INSTALL BASE APPS (UBUNTU)"
-echo "Version: 2025.2.11-920"
+echo "Version: 2025.2.18-1750"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -423,7 +423,7 @@ printLine "Ventoy"
 home_app_name="ventoy"
 home_app_subdir="$home_app_dir/$home_app_name"
 home_app_cversion="`cat "$home_app_subdir/version.txt"`"
-home_app_version="1.1.01"
+home_app_version="1.1.02"
 
 if [ "$home_app_cversion" != "$home_app_version" ]
 then
