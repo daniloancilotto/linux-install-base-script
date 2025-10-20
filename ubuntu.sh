42767caa3ff5d7907fd83b5afc9257e8e14c9ef8
Danilo Ancilotto <daniloancilotto@gmail.com>
2025-10-19 22:45:42 -0300

    Updated 4kvideodownloaderplus app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "INSTALL BASE APPS (UBUNTU)"
-echo "Version: 2025.10.01-0800"
+echo "Version: 2025.10.19-2250"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -150,7 +150,7 @@ printLine "4K Video Downloader+"
 root_app_name="4kvideodownloaderplus"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="25.3.2"
+root_app_version="25.3.3"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
